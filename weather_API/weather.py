46f738a3057c1ef2a5cdf6b499227b1b137a7bf7
Fabricio <fabriciocolombo0096@gmail.com>
2024-01-24 08:23:06 -0300

    we1

---
+++ weather_API/weather.py
@@ -0,0 +1,12 @@
+#vamos construir um projeto de previsão do tempo, pegando uma API de previsão do tempo e colocando dentro de uma interface gráfica.
+#para fazer a interface grafica vamos usar o tkinter, que é uma biblioteca padrão do python.
+#DADOS DA API >>>>
+#link do open_weather: https://openweathermap.org/
+#API_KEY = "184fd29779e314cea46590e9c90bb957"
+#BASE_URL = "https://api.openweathermap.org/data/2.5/weather")
+
+#importei 2 imagens, na pasta imagens. o arquivo superior.png vai ficar na parte de cima da interface e o arquivo
+#inferior.png vai ficar na parte de baixo da interface.
+#no meio vai ser onde vai sair o resultado da previsão do tempo.
+
+# **********BORA COMEÇAR O PROJETO**********
